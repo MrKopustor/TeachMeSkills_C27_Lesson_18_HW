acf13d0cfb5610cfb07df88f9a298017f52d6377
MrKopustor <zotop1753@yahoo.com>
2024-03-18 20:41:51 +0300

    UpDate

--- src/com/teachmeskills/lesson_18/hw/task_1/launcher/Runner.java
+++ src/com/teachmeskills/lesson_18/hw/task_1/launcher/Runner.java
@@ -1,2 +1,27 @@
-package com.teachmeskills.lesson_18.hw.task_1.launcher;public class Runner {
+package com.teachmeskills.lesson_18.hw.task_1.launcher;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.Collectors;
+
+public class Runner {
+    public static void main(String[] args) {
+        int size = 20;
+        List<Integer> list = new ArrayList<>(size);
+        for (int i = 0; i < size; i++) {
+            list.add((int) (Math.random() * 20) + 1);
+        }
+        System.out.println("Test indicator => " + list);
+
+        System.out.println("stream count => " + list.stream()
+                .count());
+
+        System.out.println("Arithmetic mean of all numbers in the stream => " + list.stream()
+                .distinct()
+                .filter(n -> n >= 7 && n <= 17 && n % 2 == 0)
+                .map(n -> n * 2)
+                .sorted()
+                .limit(4)
+                .collect(Collectors.averagingInt(Integer::intValue)));
+    }
 }
--- src/com/teachmeskills/lesson_18/hw/task_2/launcher/Runner.java
+++ src/com/teachmeskills/lesson_18/hw/task_2/launcher/Runner.java
@@ -1,2 +1,38 @@
-package com.teachmeskills.lesson_18.hw.task_2.launcher;public class Runner {
+package com.teachmeskills.lesson_18.hw.task_2.launcher;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class Runner {
+    public static void main(String[] args) {
+        List<String> list = new ArrayList<>();
+
+        list.add("Andrew");
+        list.add("Artem");
+        list.add("Alina");
+        list.add("Anastasia");
+        list.add("Artem");
+        list.add("Nikita");
+        list.add("Oleg");
+        list.add("Ivan");
+        list.add("Cyril");
+        list.add("Ruslan");
+        list.add("Eugene");
+        list.add("Paul");
+        list.add("Ruslan");
+        list.add("Ruslan");
+        list.add("alexander");
+        list.add("Daniel");
+        list.add("alexei");
+        list.add("Vladislav");
+
+        System.out.println("\"A\" names => " + list.stream()
+                .filter(n -> n.toLowerCase().startsWith("a")).toList());
+
+        System.out.print("First name => ");
+        list.stream()
+                .sorted()
+                .limit(1)
+                .forEach(System.out::println);
+    }
 }
